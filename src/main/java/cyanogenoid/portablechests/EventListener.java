902242e891a3e5f1b23d16abd774b14c9f8a501e
Giovanni Cosimo <cosimo.giovanni@outlook.it>
2021-04-12 17:41:46 +0200

    Initial commit

---
+++ src/main/java/cyanogenoid/portablechests/EventListener.java
@@ -0,0 +1,97 @@
+package cyanogenoid.portablechests;
+
+import org.bukkit.block.*;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.inventory.*;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class EventListener implements Listener {
+
+    @EventHandler
+    public void onBlockBreaks(BlockBreakEvent e) {
+        Block block = e.getBlock();
+        BlockState blockState = block.getState();
+        if (!(PortableChests.isContainer(blockState))) return;
+
+        Inventory blockInventory;
+        if (blockState instanceof Chest) blockInventory = ((Chest) blockState).getBlockInventory();
+        else blockInventory = ((Container) blockState).getInventory();
+        if (!e.isDropItems() || blockInventory.isEmpty()) return;
+
+        ItemStack blockItemStack = block.getDrops(e.getPlayer().getInventory().getItemInMainHand()).stream()
+                                        .filter(itemStack -> itemStack != null && itemStack.getType().equals(block.getType()))
+                                        .findFirst()
+                                        .orElse(null);
+        if (blockItemStack == null) return;
+
+        e.setDropItems(false);
+        block.getWorld().dropItemNaturally(block.getLocation(), PortableChests.makePortableContainer(blockInventory, blockItemStack));
+    }
+
+    @EventHandler
+    public void onBlockPlaced(BlockPlaceEvent e) {
+        Block block = e.getBlock();
+        BlockState blockState = block.getState();
+        if (!(PortableChests.isContainer(blockState))) return;
+        if (!PortableChests.isPortableContainer(e.getItemInHand())) return;
+
+        Inventory blockInventory = ((Container) blockState).getInventory();
+
+        try {
+            PortableChests.fillPortableContainer(blockInventory, e.getItemInHand());
+        } catch (InvalidConfigurationException invalidConfigurationException) {
+            e.getPlayer().sendMessage("Invalid chest content!");
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onInventoryPickupItem(InventoryPickupItemEvent e) {
+        if (!(PortableChests.isContainer(e.getInventory()) || !PortableChests.isPortableContainer(e.getItem().getItemStack()))) return;
+        if (!PortableChests.canNestItemStack(e.getItem().getItemStack())) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onInventoryMoveItem(InventoryMoveItemEvent e) {
+        if (!(PortableChests.isContainer(e.getDestination()) || !PortableChests.isPortableContainer(e.getItem()))) return;
+        if (!PortableChests.canNestItemStack(e.getItem())) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent e) {
+        if (!PortableChests.isContainer(e.getInventory())) return;
+        ItemStack itemStack = this.findMovingItemStack(e);
+        if (!PortableChests.isPortableContainer(itemStack)) return;
+        if (this.isPlayerMovingItemStackToContainer(e) && !PortableChests.canNestItemStack(itemStack, e.getWhoClicked())) e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onInventoryDrag(InventoryDragEvent e) {
+        if (!PortableChests.isContainer(e.getInventory())) return;
+        if (!PortableChests.isPortableContainer(e.getOldCursor())) return;
+        if (e.getRawSlots().stream().anyMatch(slotID -> slotID <= e.getInventory().getSize() && !PortableChests.canNestItemStack(e.getOldCursor(), e.getWhoClicked()))) e.setCancelled(true);
+    }
+
+
+    private boolean isPlayerMovingItemStackToContainer(InventoryClickEvent e) {
+        return e.getClickedInventory() != null && (e.getAction().equals(InventoryAction.MOVE_TO_OTHER_INVENTORY) && e.getClickedInventory().getType().equals(InventoryType.PLAYER)) ||
+                ((e.getAction().equals(InventoryAction.HOTBAR_MOVE_AND_READD) || e.getAction().equals(InventoryAction.HOTBAR_SWAP))  && PortableChests.isContainer(e.getClickedInventory())) ||
+                ((e.getAction().equals(InventoryAction.PLACE_ALL) || e.getAction().equals(InventoryAction.PLACE_ONE) || e.getAction().equals(InventoryAction.SWAP_WITH_CURSOR)) && PortableChests.isContainer(e.getClickedInventory()));
+    }
+
+    private ItemStack findMovingItemStack(InventoryClickEvent e) {
+        if (e.getAction().equals(InventoryAction.MOVE_TO_OTHER_INVENTORY)) return e.getCurrentItem();
+        if (e.getAction().equals(InventoryAction.HOTBAR_MOVE_AND_READD) || e.getAction().equals(InventoryAction.HOTBAR_SWAP)) return e.getWhoClicked().getInventory().getItem(e.getHotbarButton());
+        if (e.getAction().equals(InventoryAction.PLACE_ALL) || e.getAction().equals(InventoryAction.PLACE_ONE) || e.getAction().equals(InventoryAction.SWAP_WITH_CURSOR)) return e.getCursor();
+        return null;
+    }
+}
---
+++ src/main/java/cyanogenoid/portablechests/PortableChests.java
@@ -0,0 +1,125 @@
+package cyanogenoid.portablechests;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.NamespacedKey;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.*;
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+public final class PortableChests extends JavaPlugin {
+    private static NamespacedKey UNIQUE_KEY;
+    private static NamespacedKey CONTENT_KEY;
+    private static NamespacedKey NESTING_KEY;
+
+    private static boolean ALLOW_STACKING;
+    private static int MAX_NESTING;
+    private static String NESTING_LIMIT_MESSAGE;
+
+    public static Map<String, Boolean> containersConfigMap = new HashMap<>();
+
+    @Override
+    public void onEnable() {
+        this.saveDefaultConfig();
+
+        ALLOW_STACKING = this.getConfig().getBoolean("allow-stacking");
+        MAX_NESTING = this.getConfig().getInt("max-nesting");
+        NESTING_LIMIT_MESSAGE  = this.getConfig().getString("nesting-limit-message");
+
+        UNIQUE_KEY = new NamespacedKey(this, "UNIQUE");
+        CONTENT_KEY = new NamespacedKey(this, "CONTENT");
+        NESTING_KEY = new NamespacedKey(this, "NESTING");
+
+        containersConfigMap.put("Barrel", this.getConfig().getBoolean("portable-barrels"));
+        containersConfigMap.put("BlastFurnace", this.getConfig().getBoolean("portable-blast-furnaces"));
+        containersConfigMap.put("BrewingStand", this.getConfig().getBoolean("portable-brewing-stands"));
+        containersConfigMap.put("Chest", this.getConfig().getBoolean("portable-chests"));
+        containersConfigMap.put("Dispenser", this.getConfig().getBoolean("portable-dispensers"));
+        containersConfigMap.put("Dropper", this.getConfig().getBoolean("portable-droppers"));
+        containersConfigMap.put("FurnaceFurnace", this.getConfig().getBoolean("portable-furnaces"));
+        containersConfigMap.put("Hopper", this.getConfig().getBoolean("portable-hoppers"));
+        containersConfigMap.put("Smoker", this.getConfig().getBoolean("portable-smokers"));
+
+        containersConfigMap.put("DoubleChest", this.getConfig().getBoolean("portable-chests"));
+        containersConfigMap.put("ShulkerBox", false);
+
+        getServer().getPluginManager().registerEvents(new EventListener(), this);
+    }
+
+    @Override
+    public void onDisable() { }
+
+    public static ItemStack makePortableContainer(Inventory inventory, ItemStack originalItemStack) {
+        ItemStack inventoryItemStack = new ItemStack(originalItemStack);
+        ItemMeta meta = inventoryItemStack.getItemMeta();
+        if (!ALLOW_STACKING) meta.getPersistentDataContainer().set(UNIQUE_KEY, PersistentDataType.STRING, UUID.randomUUID().toString());
+        meta.getPersistentDataContainer().set(CONTENT_KEY, PersistentDataType.STRING, encodeInventory(inventory));
+        meta.getPersistentDataContainer().set(NESTING_KEY, PersistentDataType.INTEGER, getTotalContainerNesting(inventory) + 1);
+        inventoryItemStack.setItemMeta(meta);
+        inventoryItemStack.lore(Collections.singletonList(Component.text(Arrays.stream(inventory.getContents()).filter(Objects::nonNull).count() + " stacks inside").color(NamedTextColor.YELLOW)));
+        return inventoryItemStack;
+    }
+
+    public static void fillPortableContainer(Inventory blockInventory, ItemStack blockItemStack) throws InvalidConfigurationException {
+        String encodedInventory = getItemStackContentData(blockItemStack);
+        blockInventory.setContents(decodeInventory(encodedInventory, blockInventory));
+    }
+
+    public static Boolean isContainer(Object object) {
+        return containersConfigMap.getOrDefault(object.getClass().getSimpleName().replace("Craft",""), false);
+    }
+
+    public static Boolean isContainer(Inventory inventory) {
+        return inventory.getHolder() != null && isContainer(inventory.getHolder());
+    }
+
+    public static Boolean isPortableContainer(ItemStack item) {
+        return getItemStackContentData(item) != null;
+    }
+
+    public static Boolean canNestItemStack(ItemStack itemStack) { return getItemStackNestingData(itemStack) < MAX_NESTING; }
+    public static Boolean canNestItemStack(ItemStack itemStack, HumanEntity player) {
+        if (canNestItemStack(itemStack)) return true;
+        if (!NESTING_LIMIT_MESSAGE.isEmpty()) player.sendMessage(NESTING_LIMIT_MESSAGE);
+        return false;
+    }
+
+    private static String encodeInventory(Inventory inventory) {
+        YamlConfiguration config = new YamlConfiguration();
+        IntStream.range(0, inventory.getSize()).forEach(index -> config.set(Integer.toString(index), inventory.getContents()[index]));
+        return config.saveToString();
+    }
+
+    private static ItemStack[] decodeInventory(String encodedInventory, Inventory inventory) throws InvalidConfigurationException {
+        YamlConfiguration config = new YamlConfiguration();
+        config.loadFromString(encodedInventory);
+
+        ItemStack[] content = new ItemStack[inventory.getSize()];
+        IntStream.range(0, inventory.getSize()).forEach(index -> content[index] = config.getItemStack(Integer.toString(index), null));
+
+        return content;
+    }
+
+    private static String getItemStackContentData(ItemStack item) {
+        if (item == null || item.getItemMeta() == null || !item.getItemMeta().getPersistentDataContainer().has(CONTENT_KEY, PersistentDataType.STRING)) return null;
+        return item.getItemMeta().getPersistentDataContainer().get(CONTENT_KEY, PersistentDataType.STRING);
+    }
+
+    private static Integer getItemStackNestingData(ItemStack item) {
+        if (item == null || item.getItemMeta() == null || !item.getItemMeta().getPersistentDataContainer().has(NESTING_KEY, PersistentDataType.INTEGER)) return -1;
+        return item.getItemMeta().getPersistentDataContainer().get(NESTING_KEY, PersistentDataType.INTEGER);
+    }
+
+    private static Integer getTotalContainerNesting(Inventory inventory) {
+        return Arrays.stream(inventory.getContents()).filter(PortableChests::isPortableContainer).map(PortableChests::getItemStackNestingData).max(Comparator.naturalOrder()).orElse(-1);
+    }
+}
