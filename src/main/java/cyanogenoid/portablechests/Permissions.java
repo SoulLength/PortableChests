a34e6ab9df0c4c27f7d25dc1834eae6880cd532e
Giovanni Cosimo <cosimo.giovanni@outlook.it>
2021-05-25 17:41:57 +0200

    added penalty skip permission

--- src/main/java/cyanogenoid/portablechests/Permissions.java
+++ src/main/java/cyanogenoid/portablechests/Permissions.java
@@ -2,4 +2,5 @@ package cyanogenoid.portablechests;
 
 public class Permissions {
     public static final String canCreatePortableChest = "cyanogenoid.portablechests.create";
+    public static final String skipPenalty = "cyanogenoid.portablechests.penalty.skip";
 }
