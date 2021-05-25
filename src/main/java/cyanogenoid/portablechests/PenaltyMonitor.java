29bcb5f37045f36a8d6cf20e29498b1559b838d7
Giovanni Cosimo <cosimo.giovanni@outlook.it>
2021-05-25 17:41:44 +0200

    added penalties support

---
+++ src/main/java/cyanogenoid/portablechests/PenaltyMonitor.java
@@ -0,0 +1,39 @@
+package cyanogenoid.portablechests;
+
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Collection;
+import java.util.Objects;
+
+import static org.bukkit.Bukkit.getOnlinePlayers;
+
+public class PenaltyMonitor extends BukkitRunnable {
+    private final Collection<PotionEffect> penalties;
+
+    PenaltyMonitor(Collection<PotionEffect> penalties) {
+        this.penalties = penalties;
+    }
+
+    @Override
+    public void run() {
+        getOnlinePlayers().stream()
+                .filter(player -> !player.hasPermission(Permissions.skipPenalty))
+                .map(HumanEntity::getInventory)
+                .filter(PortableChests::containsPortableContainer)
+                .map(PlayerInventory::getHolder)
+                .filter(Objects::nonNull)
+                .forEach(this::applyPenalties);
+    }
+
+    private void applyPenalties(HumanEntity humanEntity) {
+        penalties.forEach(penalty -> addPenalty(humanEntity, penalty));
+
+    }
+
+    private void addPenalty(HumanEntity humanEntity, PotionEffect penalty) {
+        humanEntity.addPotionEffect(penalty);
+    }
+}
--- src/main/java/cyanogenoid/portablechests/PortableChests.java
+++ src/main/java/cyanogenoid/portablechests/PortableChests.java
@@ -7,12 +7,16 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.*;
 import java.util.Comparator;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 
@@ -54,6 +58,15 @@ public final class PortableChests extends JavaPlugin {
 
         getServer().getPluginManager().registerEvents(new BlockListener(), this);
         getServer().getPluginManager().registerEvents(new InventoryListener(), this);
+
+        String[] penalties = this.getConfig().getStringList("penalties").toArray(new String[0]);
+        Integer[] amplifiers = this.getConfig().getIntegerList("penalties-amplifiers").toArray(new Integer[0]);
+        if (penalties.length == 0) return;
+
+        new PenaltyMonitor(IntStream.range(0, penalties.length)
+                .mapToObj(index -> new PotionEffect(Objects.requireNonNull(PotionEffectType.getByName(penalties[index])), this.getConfig().getInt("penalty-duration"), index < amplifiers.length ? amplifiers[index] : 0))
+                .collect(Collectors.toList()))
+                .runTaskTimer(this, 0, this.getConfig().getInt("penalty-update"));
     }
 
     @Override
@@ -88,9 +101,10 @@ public final class PortableChests extends JavaPlugin {
         return inventory.getHolder() != null && isContainer(inventory.getHolder());
     }
 
-    public static Boolean isPortableContainer(ItemStack item) {
-        return getItemStackNestingData(item) != -1;
+    public static Boolean isPortableContainer(ItemStack itemStack) {
+        return getItemStackNestingData(itemStack) != -1;
     }
+    public static Boolean containsPortableContainer(Inventory inventory) { return Arrays.stream(inventory.getContents()).anyMatch(PortableChests::isPortableContainer); }
 
     public static Boolean canNestItemStack(ItemStack itemStack) { return getItemStackNestingData(itemStack) < MAX_NESTING; }
     public static Boolean canNestItemStack(ItemStack itemStack, HumanEntity player) {
