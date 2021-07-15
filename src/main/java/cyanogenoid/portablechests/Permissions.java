c7736944254adb29e602ad93364483d45d0e5879
Giovanni Cosimo <cosimo.giovanni@outlook.it>
2021-07-16 01:08:30 +0200

    Added enchantment.skip permission

--- src/main/java/cyanogenoid/portablechests/Permissions.java
+++ src/main/java/cyanogenoid/portablechests/Permissions.java
@@ -2,5 +2,6 @@ package cyanogenoid.portablechests;
 
 public class Permissions {
     public static final String canCreatePortableChest = "cyanogenoid.portablechests.create";
-    public static final String skipPenalty = "cyanogenoid.portablechests.penalty.skip";
+    public static final String canSkipPenalty = "cyanogenoid.portablechests.penalty.skip";
+    public static final String canSkipEnchantment = "cyanogenoid.portablechests.enchantment.skip";
 }
