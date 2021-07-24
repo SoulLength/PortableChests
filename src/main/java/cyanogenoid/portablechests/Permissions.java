b2f149560707a8cf9b9fdd9b0cd3558d9f1fa90f
Giovanni Cosimo <cosimo.giovanni@outlook.it>
2021-07-24 16:53:54 +0200

    Added create.anywhere and place.anywhere permissions

--- src/main/java/cyanogenoid/portablechests/Permissions.java
+++ src/main/java/cyanogenoid/portablechests/Permissions.java
@@ -1,7 +1,9 @@
 package cyanogenoid.portablechests;
 
 public class Permissions {
-    public static final String canCreatePortableChest = "cyanogenoid.portablechests.create";
+    public static final String canCreatePortableContainers = "cyanogenoid.portablechests.create";
+    public static final String canCreatePortableContainersAnywhere = "cyanogenoid.portablechests.create.anywhere";
+    public static final String canPlacePortableContainersAnywhere = "cyanogenoid.portablechests.place.anywhere";
     public static final String canSkipPenalty = "cyanogenoid.portablechests.penalty.skip";
     public static final String canSkipEnchantment = "cyanogenoid.portablechests.enchantment.skip";
 }
